20a1f3b357ed01031fd2ae4fdae815ce1a2f21d4
Henry Tito <140865436+HenryTito@users.noreply.github.com>
2025-11-30 20:58:02 -0300

    remoção migrations
    remoção migrations

--- backend/prisma/migrations/20251129181323_add_unique_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[codigo]` on the table `Aeronave` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `Aeronave_codigo_key` ON `Aeronave`(`codigo`);
